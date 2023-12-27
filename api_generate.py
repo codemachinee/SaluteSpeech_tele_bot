751754d480761ba68ba4677583391fb9114ceccd
codemachinee <johnybritva2@gmail.com>
2023-12-27 15:55:46 +0300

    незначительные изменения

--- api_generate.py
+++ api_generate.py
@@ -1,13 +1,13 @@
 import requests
 import uuid
-from paswords import autoriz_data
+from paswords import autoriz_data_salute
 
 
 def key_generate():
     url = 'https://ngw.devices.sberbank.ru:9443/api/v2/oauth'
 
     headers = {
-        'Authorization': f'Basic {autoriz_data}',
+        'Authorization': f'Basic {autoriz_data_salute}',
         'RqUID': str(uuid.uuid4()),
         'Content-Type': 'application/x-www-form-urlencoded'
     }
